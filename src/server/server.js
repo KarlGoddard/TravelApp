c2708410ed6908e0fec4656903ec441a6a3ebdf2
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-19 22:16:23 +0000

    feat: create client/index.js and client/js/*.js files.  set index.js as start in webpack config

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,7 @@ var path = require('path')
 const express = require('express')
 
 const app = express()
-app.use(express.static('client'))
+app.use(express.static('dist'))
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
