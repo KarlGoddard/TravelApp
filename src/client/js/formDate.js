a6b950ab2835a02287b7bce6129c8bff377c5714
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-11-28 17:12:38 +0000

    feat: testing function to set min(today) and max(+30days) for input

--- src/client/js/formDate.js
+++ src/client/js/formDate.js
@@ -2,21 +2,47 @@ function minToday() {
 
   //event.preventDefault();
 
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1;
-  var yyyy = today.getFullYear();
-   if(dd<10){
-          dd='0'+dd
-      }
-      if(mm<10){
-          mm='0'+mm
-      }
+  // let today = new Date();
+  // let dd = today.getDate();
+  // let mm = today.getMonth()+1;
+  // let yyyy = today.getFullYear();
+  //
+  // if(dd<10){
+  //         dd='0'+dd
+  //     }
+  //     if(mm<10){
+  //         mm='0'+mm
+  //     }
+  //
+  // today = yyyy+'-'+mm+'-'+dd;
+  // document.getElementById("depDate").setAttribute("min", today);
 
-  today = yyyy+'-'+mm+'-'+dd;
-  document.getElementById("depDate").setAttribute("min", today);
+  let minDate = new Date();
+  minDate.setDate(minDate.getDate());
+  let maxDate = new Date();
+  maxDate = minDate;
 
-  //set max date 3 months ahead
+  let minDateFormat = formatDate(minDate);
+  let maxDateFormat = formatDate(addDays(maxDate,30));
+
+  // function formatDate(date) {
+  //     return (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
+  // }
+
+  function formatDate(date) {
+      return date.getFullYear() + '-' + (date.getMonth() +1) + '-' + date.getDate();
+  }
+
+  function addDays(date, days) {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+document.getElementById("depDate").setAttribute("min", minDateFormat);
+document.getElementById("depDate").setAttribute("max", maxDateFormat);
+// document.getElementById('ccc').innerHTML = minDate;
+// document.getElementById('bbb').innerHTML = maxDate;
 
 }
 
