949f7c2c2b910c195d323a0c66ed398e1ed8bf98
Karl Goddard <karlgoddard@hotmail.co.uk>
2020-12-03 15:54:12 +0000

    feat: fix previously missed error with days difference js
    needed to set times to default of 0 to ensure, different times weren't 
    impacting the date difference calculation

--- src/client/js/DaysToDeparture.js
+++ src/client/js/DaysToDeparture.js
@@ -1,13 +1,14 @@
 function daysToDeparture(dte) {
 
   console.log('Departure date is ' + dte);
-  let inputDate = new Date(dte);
-  let currentDate = new Date();
+  let inputDate = new Date(dte).setHours(0,0,0,0);
+  let currentDate = new Date().setHours(0,0,0,0);
 
-  let timeDiff = Math.abs(inputDate.getTime() - currentDate.getTime());
-  let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  let timeDiff = Math.abs(inputDate - currentDate);
+  let diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
 
-  console.log('Days to departure is ' + diffDays);
+  console.log('Days to departure is ' + (diffDays));
+
   return diffDays;
 }
 
--- src/client/js/form.js
+++ src/client/js/form.js
@@ -7,33 +7,37 @@ function handleSubmit(event) {
   let countryCode = document.getElementById("inputCountry").value;
   let depDate = document.getElementById("depDate").value;
   let bodyText = `name_equals=${cityName}&countryCode=${countryCode}`;
+  let daysToGo = Client.daysToDeparture(depDate);
 
   if (Client.inputChecker(cityName,depDate)) {
   console.log('input check OK');
-  document.getElementById('inputCheck').innerHTML = `Thank you ${depDate}`;
+      if (daysToGo < 1) {
+            document.getElementById('inputCheck').innerHTML = `Thank you.  You are travelling today`;
+      } else {
+            document.getElementById('inputCheck').innerHTML = `Thank you, you depart for ${cityName} in ${daysToGo} day(s)`;
+      }
 
+          fetch('http://localhost:8082/geo',{
+          method: "POST",
+          credentials: 'same-origin',
+          headers: {
+          'Content-Type': 'text/plain'
+          },
+          body: bodyText
+          })
+          .then((res) => res.json())
+          .then(function(res) {
 
-      fetch('http://localhost:8082/geo',{
-      method: "POST",
-      credentials: 'same-origin',
-      headers: {
-      'Content-Type': 'text/plain'
-      },
-      body: bodyText
-      })
-      .then((res) => res.json())
-      .then(function(res) {
-           let daysToGo = Client.daysToDeparture(depDate);
-           if (daysToGo < 8) {
-             Client.getWeather(res)
-           } else {
-           //document.getElementById('bbb').innerHTML = res.geonames[0].lng
-             Client.getForecast(res);
-           };
-        })
-        .catch((error) =>{
-          console.log(error);
-        });
+               if (daysToGo < 7) {
+                 Client.getWeather(res)
+               } else {
+               //document.getElementById('bbb').innerHTML = res.geonames[0].lng
+                 Client.getForecast(res);
+               };
+            })
+            .catch((error) =>{
+              console.log(error);
+            });
 
   } else {
   console.log ('input check NOT OK');
